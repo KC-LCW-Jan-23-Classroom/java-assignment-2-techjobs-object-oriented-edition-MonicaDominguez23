3d59a9826eae10aefa7451076e597aae087094e1
MonicaDominguez23 <120822003+MonicaDominguez23@users.noreply.github.com>
2023-07-19 16:44:05 -0500

    Add files via upload

---
+++ java-assignment-2-techjobs-object-oriented-edition-MonicaDominguez23-master/src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -0,0 +1,35 @@
+package org.launchcode.techjobs.oo;
+
+//import static sun.security.pkcs11.wrapper.Functions.getId;
+
+public abstract class JobField {
+
+    private int id;
+    private static int nextId = 1;
+    private String value;
+
+    public JobField() {
+        id = nextId;
+        nextId++;
+    }
+
+    public JobField(String value) {
+        this();
+        this.value = value;
+    }
+
+    // Custom toString, equals, and hashCode methods:
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof JobField)) return false;
+        JobField that = (JobField) o;
+        return id == that.id;
+    }
+}
